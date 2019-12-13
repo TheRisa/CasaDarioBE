f116bdbd3662da09dfa75ea1f14982ed39fdd4cf
TheRisa <dariorisa@hotmail.it>
2019-12-13 16:57:09 +0100

    host

--- admin/settings.py
+++ admin/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = ')4p0!2ylp3_ui+lt4qx$zt_o!tasuyt+yfs*$ouscoheslm=t2'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = [polar-tundra-64747.herokuapp.com]
 
 
 # Application definition
