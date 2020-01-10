85612da478bd2e3bc670bd6b9843f32ea71c423b
TheRisa <dariorisa@hotmail.it>
2020-01-10 17:46:55 +0100

    tmp

--- admin/settings.py
+++ admin/settings.py
@@ -110,22 +110,22 @@ WSGI_APPLICATION = 'admin.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/2.0/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.mysql',
-        'NAME': 'casadario',
-        'USER': 'admincasadario',
-        'PASSWORD': 'admin1832',
-        'HOST': 'db4free.net',
-        'PORT': '3306',
-    }
-}
 # DATABASES = {
 #     'default': {
-#         'ENGINE': 'django.db.backends.sqlite3',
-#         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+#         'ENGINE': 'django.db.backends.mysql',
+#         'NAME': 'casadario',
+#         'USER': 'admincasadario',
+#         'PASSWORD': 'admin1832',
+#         'HOST': 'db4free.net',
+#         'PORT': '3306',
 #     }
 # }
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+    }
+}
 
 
 # Password validation
