a1285ea0c55633735f01651a31ce98930aad3259
TheRisa <dariorisa@hotmail.it>
2020-01-11 12:35:58 +0100

    test

--- event/views.py
+++ event/views.py
@@ -28,7 +28,7 @@ def getAllEvents(request, userNameInput):
     response = []
     for invite in invites:
         tmpEvent = {
-            'id': invite.event.event_id,
+            'id': invite.event.id,
             'name': invite.event.name,
             'description': invite.event.description,
             'place': invite.event.place,
