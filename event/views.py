30d30ccfce39a73bb8445607afba18c4b9d8cb9e
drisaliti <d.risaliti@quidinfo.it>
2020-01-22 16:25:56 +0100

    db

--- event/views.py
+++ event/views.py
@@ -43,7 +43,7 @@ def getAllEvents(request, userNameInput):
             'isConfirmed': invite.isConfirmed
         }
         response.append(tmpEvent)
-    return JsonResponse({'response': response, 'user': user.userName})
+    return JsonResponse({'user': user.userName})
 
 
 @api_view(['POST'])
