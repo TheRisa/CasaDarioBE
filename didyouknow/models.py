1022b674254d617c659b1d6bb9875b9a1befd712
drisaliti <d.risaliti@quidinfo.it>
2020-01-22 11:54:56 +0100

    db

--- didyouknow/models.py
+++ didyouknow/models.py
@@ -3,7 +3,7 @@ from django.db import models
 
 class Curiosity(models.Model):
     curiosityText = models.TextField(
-        "Curiosity text", blank=False, max_length=200, unique=True)
+        "Curiosity text", blank=False, unique=True)
 
     def __str__(self):
         return self.curiosityText
