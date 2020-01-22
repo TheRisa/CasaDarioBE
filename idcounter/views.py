f219ae0bf850426ca8994a3f1acf71cd834ef830
drisaliti <d.risaliti@quidinfo.it>
2020-01-22 14:53:01 +0100

    db

--- idcounter/views.py
+++ idcounter/views.py
@@ -62,28 +62,23 @@ def getEvent(request):
 
 def incrementEvent(request):
     # Metodo mysql
-    try:
-        collection = IdCollection.objects.get(id=1)
-        collection.id_event = collection.id_event + 1
-        collection.save()
-    except (IdCollection.DoesNotExist, DatabaseError):
-        return JsonResponse({'response': False})
-    return JsonResponse({'response': collection.id_event})
-
-    # Metodo mongodb
     # try:
     #     collection = IdCollection.objects.get(id=1)
-    #     myclient = MongoClient(
-    #         "mongodb+srv://TheRisa:admin1832@casadario-kzgcj.mongodb.net/test?retryWrites=true&w=majoritys")
-    #     mydb = myclient["casadario"]
-    #     mycol = mydb["idcounter_idcollection"]
-    #     newCount = collection.id_event + 1
-    #     mycol.update_one(
-    #         collection, {"id": 1, "id_event": newCount, "id_user": collection.id_user,
-    #                      "id_invite": collection.id_invite})
-    # except BulkWriteError as bwe:
-    #     return JsonResponse({'response': bwe.details["nInserted"] > 0})
-    # return JsonResponse({'response': True})
+    #     collection.id_event = collection.id_event + 1
+    #     collection.save()
+    # except (IdCollection.DoesNotExist, DatabaseError):
+    #     return JsonResponse({'response': False})
+    # return JsonResponse({'response': collection.id_event})
+
+    # Metodo mongodb
+    try:
+        mycol = conncet()
+        collection = mycol.find_one()
+        mycol.update_one(
+            {"collection_id": 1}, {"$set": {"id_event": collection["id_event"] + 1}})
+    except BulkWriteError as bwe:
+        return JsonResponse({'response': bwe.details["nInserted"] > 0})
+    return JsonResponse({'response': True})
 
 
 def getInvite(request):
@@ -97,25 +92,20 @@ def getInvite(request):
 
 def incrementInvite(request):
     # Metodo mysql
-    try:
-        collection = IdCollection.objects.get(id=1)
-        collection.id_invite = collection.id_invite + 1
-        collection.save()
-    except (IdCollection.DoesNotExist, DatabaseError):
-        return JsonResponse({'response': False})
-    return JsonResponse({'response': collection.id_invite})
-
-    # Metodo mongodb
     # try:
     #     collection = IdCollection.objects.get(id=1)
-    #     myclient = MongoClient(
-    #         "mongodb+srv://TheRisa:admin1832@casadario-kzgcj.mongodb.net/test?retryWrites=true&w=majoritys")
-    #     mydb = myclient["casadario"]
-    #     mycol = mydb["idcounter_idcollection"]
-    #     newCount = collection.id_invite + 1
-    #     mycol.update_one(
-    #         collection, {"id": 1, "id_invite": newCount, "id_user": collection.id_user,
-    #                      "id_event": collection.id_event})
-    # except BulkWriteError as bwe:
-    #     return JsonResponse({'response': bwe.details["nInserted"] > 0})
-    # return JsonResponse({'response': True})
+    #     collection.id_invite = collection.id_invite + 1
+    #     collection.save()
+    # except (IdCollection.DoesNotExist, DatabaseError):
+    #     return JsonResponse({'response': False})
+    # return JsonResponse({'response': collection.id_invite})
+
+    # Metodo mongodb
+    try:
+        mycol = conncet()
+        collection = mycol.find_one()
+        mycol.update_one(
+            {"collection_id": 1}, {"$set": {"id_invite": collection["id_invite"] + 1}})
+    except BulkWriteError as bwe:
+        return JsonResponse({'response': bwe.details["nInserted"] > 0})
+    return JsonResponse({'response': True})
